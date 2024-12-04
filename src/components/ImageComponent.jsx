7440b90d424581b765d8d6d2814853633e6879dd
AnjaliDebnath <anjalidebnath1003@gmail.com>
2024-12-04 16:09:44 +0530

    imageComponent added

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./ImageComponent.css";
 
-const ImageComponent = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+const ImageComponent = ({ image, onUpdate }) => {
+  const [loading, setLoading] = useState(false);
+  const [retryCount, setRetryCount] = useState(0);
+
+  useEffect(() => {
+    if (image.error && retryCount < 3) {
+      setLoading(true);
+      const timer = setTimeout(() => {
+        const success = Math.random() > 0.5; // to find  success/failure
+        setLoading(false);
+        if (success) {
+          onUpdate({ ...image, error: false, ready: true });
+        } else {
+          setRetryCount(retryCount + 1);
+        }
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [image.error, retryCount, image, onUpdate]);
+
+  const renderContent = () => {
+    if (loading) {
+       return <div> </div>
+    }
+
+    if (image.error) {
+      return <div ></div>;
+    }
+
+    if (image.ready) {
+      return <img src={image.url} alt="Loaded"  />;
+    }
+
+    return <div className="placeholder">Placeholder</div>;
+  };
+
+  return <div className="image-container">{}</div>;
+};
 
 export default ImageComponent;
